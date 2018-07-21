9520dd273195bfee285affe0f21261fdf6cd0669
maryadi <maryadi071@gmail.com>
2018-07-21 11:44:56 +0700

    apotik surya

---
+++ src/apotik_surya/Apotik_surya.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package apotik_surya;
+
+/**
+ *
+ * @author altec
+ */
+public class Apotik_surya {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        halaman_utama buka = new halaman_utama();
+        buka.setVisible(true);
+    }
+    
+}
